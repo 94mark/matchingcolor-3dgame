6e2e9c124e6bab1bb0128bb7b767494669acb766
94mark <90877724+94mark@users.noreply.github.com>
2022-03-22 11:34:22 +0900

    Update CubeSelector.cs

---
+++ matchingcolor/Assets/Scripts/CubeSelector.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSelector : MonoBehaviour
+{
+    private ObjectDetector objectDetector;
+
+    private void Awake()
+    {
+        objectDetector = GetComponent<ObjectDetector>();
+        objectDetector.raycastEvent.AddListener(SelectCube);
+    }
+
+    public void SelectCube(Transform hit)
+    {
+        hit.GetComponent<CubeController>().ChangeColor();
+    }
+}
